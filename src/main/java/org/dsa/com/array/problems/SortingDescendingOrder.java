c28d57b189674f2da0a5ff603e87b44bcf14d425
ialexjx <akshathrx6393@gmail.com>
2023-09-20 15:29:24 +0530

    Sort an array of integers in descending order

---
+++ src/main/java/org/dsa/com/array/problems/SortingDescendingOrder.java
@@ -0,0 +1,21 @@
+package org.dsa.com.array.problems;
+
+public class SortingDescendingOrder {
+    public static void main(String[] args) {
+        int array[] = {12,32,112,45,156,45,45,90};
+        int temp = 0;
+        for(int i=0; i< array.length; i++){
+            for(int j=i+1; j<array.length; j++){
+                if(array[i] < array[j]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        for(int i : array){
+            System.out.print(i + " ");
+        }
+    }
+}
