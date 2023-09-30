4391c9d11cb0584c9f8396123dff5fb2755bd3d0
ialexjx <akshathrx6393@gmail.com>
2023-10-01 01:30:11 +0530

    Printing current date in java using Date class

---
+++ src/main/java/org/dsa/com/date/problems/PrintCurrentDate.java
@@ -0,0 +1,9 @@
+package org.dsa.com.date.problems;
+
+import java.util.Date;
+
+public class PrintCurrentDate {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+    }
+}
---
+++ src/main/java/org/dsa/com/number/problems/Time.java
@@ -0,0 +1,11 @@
+package org.dsa.com.number.problems;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Time {
+    public static void main(String[] args) {
+
+        System.out.println(new Date());
+    }
+}
