4882990b2e54828c31bc252cdd68c573906baa93
Akshat Jaiswal <125273100+ialexjx@users.noreply.github.com>
2024-01-03 23:46:31 +0530

    Update BubbleSort.java

--- src/main/java/org/dsa/com/sorting/algo/problems/BubbleSort.java
+++ src/main/java/org/dsa/com/sorting/algo/problems/BubbleSort.java
@@ -1,16 +1,37 @@
 package org.dsa.com.sorting.algo.problems;
+import java.util.Arrays;
 
 public class BubbleSort {
-    public static void main(String[] args) {
-        int array[] = {12, 32, 14, 65, 91, 23, 30};
+      public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
 
-        //compare the adjacent elements of array and swap
-         for(int i=0; i< array.length; i++){
-            for(int j=i+1; j< array.length; j++){
-                if(array[j]>array[i]){
-                    System.out.println();
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
                 }
             }
-         }
+
+            // If no swaps occurred, the array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
     }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
 }
